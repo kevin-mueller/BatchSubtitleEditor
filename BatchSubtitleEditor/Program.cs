e0784e1bf02e4568cbca98f5f5523f1d8fe3d5a8
Kevin Müller <kevin.mueller1@gmx.ch>
2020-02-01 18:49:18 +0100

    Finished main functionality

--- BatchSubtitleEditor/Program.cs
+++ BatchSubtitleEditor/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using SubtitlesParser.Classes;
+using SubtitlesParser.Classes.Parsers;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace BatchSubtitleEditor
 {
@@ -7,17 +11,73 @@ namespace BatchSubtitleEditor
     {
         static void Main(string[] args)
         {
+
+
+
             Console.WriteLine("-------Welcome To Batch Subtitle Editor-------");
             Console.WriteLine("\n" +
                 "Enter the path to the folder containing your subtitles." +
                 "\n" +
-                "The program will automatically search for .srt files.");
+                "The program will automatically search for .srt files. (Also scanning for subdirectories)");
             var path = Console.ReadLine();
 
-            var files = Directory.GetFiles(path, "*.srt");
-            foreach (var file in files)
+            Console.WriteLine("Enter the timeshift you want to apply: (in milliseconds) ");
+            var timeshiftString = Console.ReadLine();
+            int timeShift = 0;
+            try
             {
+                timeShift = Convert.ToInt32(timeshiftString);
+            } catch
+            {
+                Console.WriteLine("Please enter a number.");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+
+            var files = Directory.GetFiles(path, "*.srt", SearchOption.AllDirectories);
+            foreach (var filePath in files)
+            {
+                var parser = new SubParser();
+                List<SubtitleItem> itemsShifted = null;
+                List<SubtitleItem> itemsOriginal = new List<SubtitleItem>();
+                using (var fileStream = File.OpenRead(filePath))
+                {
+                    try
+                    {
+                        itemsShifted = parser.ParseStream(fileStream);
+
+                        //Copy to original list without copying the reference
+                        foreach (var _ in itemsShifted)
+                        {
+                            itemsOriginal.Add(new SubtitleItem()
+                            {
+                                EndTime = _.EndTime,
+                                StartTime = _.StartTime,
+                                Lines = _.Lines
+                            });
+                        }
+
+
+                        Console.WriteLine($"Shifting Subtitle: {Path.GetFileName(filePath)} by {timeShift} milliseconds.");
+                        foreach (var item in itemsShifted)
+                        {
+                            item.StartTime += timeShift;
+                            item.EndTime += timeShift;
+                        }
+                        
+                    }
+                    catch
+                    {
+                        Console.WriteLine($"Not a valid subtitle format: {filePath}");
+                    }
+                }
+                parser = null;
                 
+                var res = SubtitleEditor.SaveItemsToFile(itemsShifted, itemsOriginal, filePath);
+                if (!res)
+                    Console.WriteLine("Error writing to subtitle.");
+                else Console.WriteLine("Subtitle successfully edited!");
+
             }
 
             
---
+++ BatchSubtitleEditor/SubtitleEditor.cs
@@ -0,0 +1,39 @@
+﻿using SubtitlesParser.Classes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BatchSubtitleEditor
+{
+    public static class SubtitleEditor
+    {
+        public static bool SaveItemsToFile(List<SubtitleItem> itemsShifted, List<SubtitleItem> itemsOriginal, string filePath)
+        {
+            var fileContent = File.ReadAllText(filePath);
+            var originalFileContent = fileContent;
+
+            for (int i = 0; i < itemsShifted.Count; i++)
+            {
+                var originalItem = itemsOriginal[i];
+
+                var searchString = ConvertItemToValidString(originalItem);
+
+                fileContent = fileContent.Replace(searchString, ConvertItemToValidString(itemsShifted[i]));
+            }
+
+            File.WriteAllText(filePath, fileContent);
+
+            return !fileContent.Equals(originalFileContent);
+        }
+
+        private static string ConvertItemToValidString(SubtitleItem item)
+        {
+            var startTime = TimeSpan.FromMilliseconds(item.StartTime).ToString().Replace(".", ",");
+            startTime = startTime[0..^4];
+            var endTime = TimeSpan.FromMilliseconds(item.EndTime).ToString().Replace(".", ",");
+            endTime = endTime[0..^4];
+            return startTime + " --> " + endTime;
+        }
+    }
+}
