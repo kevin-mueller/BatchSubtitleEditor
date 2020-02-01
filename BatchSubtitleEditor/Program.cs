33d483017c2a4f1f29309b4c7cb893772a6e151a
Kevin Müller <kevin.mueller1@gmx.ch>
2020-02-01 17:39:28 +0100

    Projektdateien hinzufügen.

---
+++ BatchSubtitleEditor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace BatchSubtitleEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------Welcome To Batch Subtitle Editor-------");
+            Console.WriteLine("\n" +
+                "Enter the path to the folder containing your subtitles." +
+                "\n" +
+                "The program will automatically search for .srt files.");
+            var path = Console.ReadLine();
+
+            var files = Directory.GetFiles(path, "*.srt");
+            foreach (var file in files)
+            {
+                
+            }
+
+            
+        }
+    }
+}
